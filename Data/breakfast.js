8d1c1ef8ec7e842774914f18f01d729993a95073
bmeinert8 <brian.meinert1@gmail.com>
2025-01-20 13:51:50 -0500

    add breakfast recipes with images and descriptions data file

---
+++ Data/breakfast.js
@@ -0,0 +1,114 @@
+const breakfast = [
+  {
+    image: "./images/breakfast/cinnabon-rolls.png",
+    name: "Cinnabon Cinnamon Rolls",
+    description: "This recipe recreates the iconic, gooey, and delicious sweet rolls, topped with rich creamy frosting and bursting with cinnamon-spiced delight.",
+    author: "Marsha Fernandez",
+    keywords: [
+      "cinnamon rolls",
+      "cinnabon",
+      "sweet rolls",
+      "cinnamon"
+    ]
+  },
+  {
+    image: "./images/breakfast/ihop-pancake.png",
+    name: "IHOP Buttermilk Pancakes",
+    description: "This recipe captures the essence of fluffy, golden-brown pancakes with a touch of sweetness and a perfect buttermilk tang.",
+    author: "Holly",
+    keywords: [
+      "ihop",
+      "IHOP",
+      "pancakes",
+      "pancake",
+      "buttermilk",
+      "buttermilk pancakes",
+    ]
+  },
+  {
+    image: "./images/breakfast/cb-pancakes.png",
+    name: "Sweet Potato Banana Pancakes",
+    description: "This recipe blends the natural sweetness of bananas with the hearty goodness of sweet potatoes, creating fluffy, nutrient-packed pancakes.",
+    author: "The Healthy Hulk",
+    keywords: [
+      "sweet potato",
+      "pancakes",
+      "cracker barrel",
+      "cracker barrel pancakes",
+      "banana",
+    ]
+  },
+  {
+    image: "./images/breakfast/sb-lemon-loaf.png",
+    name: "Starbucks Lemon Loaf",
+    description: "This recipe delivers a moist, zesty cake bursting with lemon flavor and topped with a tangy glaze, recreating the beloved café treat.",
+    author: "SB",
+    keywords: [
+      "Starbucks",
+      "starbucks",
+      "lemon loaf",
+      "lemon",
+      "cake",
+      "lemon cake",
+      "loaf"
+    ]
+  },
+  {
+    image: "./images/breakfast/sb-pumpkin-bread.png",
+    name: "Starbucks Pumpkin Bread",
+    description: "This recipe serves up a moist, warmly spiced loaf with the perfect balance of pumpkin flavor and sweetness, just like the seasonal favorite.",
+    author: "stephanie Manley",
+    keywords: [
+      "Starbucks",
+      "starbucks",
+      "pumpkin bread",
+      "pumpkin loaf",
+      "pumpkin",
+      "bread",
+      "loaf"
+    ]
+  },
+  {
+    image: "./images/breakfast/cb-hashbrown-casserole.png",
+    name: "Cracker Barrel Hashbrown Casserole",
+    description: "This recipe recreates the comforting, cheesy, and creamy dish with tender shredded potatoes, making it the ultimate indulgent side.",
+    author: "Marbalet",
+    keywords: [
+      "Cracker Barrel",
+      "cracker barrel",
+      "hashbrown casserole",
+      "hashbrown",
+      "casserole"
+    ]
+  },
+  {
+    image: "./images/breakfast/be-biscuits-gravy.png",
+    name: "Bob Evans Biscuits and Gravy",
+    description: "This recipe delivers fluffy, buttery biscuits smothered in rich, creamy sausage gravy, capturing the hearty comfort of the breakfast classic.",
+    author: "Arielle Lambert",
+    keywords: [
+      "Bob Evans",
+      "bob evans",
+      "bob",
+      "evans",
+      "biscuits and gravy",
+      "biscuits",
+      "gravy"
+    ]
+  },
+  {
+    image: "./images/breakfast/fw-bacon.png",
+    name: "First Watch Million Dollar Bacon",
+    description: "This recipe offers sweet, spicy, and caramelized thick cut bacon that;s baked to crispy perfection, making it a truly indulgent treat.",
+    author: "Arielle Lambert",
+    keywords: [
+      "First Watch",
+      "first watch",
+      "million dollar bacon",
+      "bacon",
+      "million dollar",
+      "million",
+      "dollar"
+    ]
+  }
+];
