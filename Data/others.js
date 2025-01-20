0ab6890499852b03ee1d208596580932e88101e6
bmeinert8 <brian.meinert1@gmail.com>
2025-01-20 16:10:39 -0500

    add desserts data file with recipes, images, and descriptions

---
+++ Data/others.js
@@ -0,0 +1,115 @@
+ const  desserts = [
+  { 
+    image: "./images/desserts/cb-apple.png",
+    published: "20 October 2024",
+    name: "Cracker Barrel Apple Dumplings",
+    description: "This recipe offers tender, cinnamon-spiced apples wrapped in flaky pastry, backed to golden perfection and drizzled with a sweet, buttery sauce.",
+    author: "Chris Shields",
+    keywords: [
+      "cracker barrel",
+      "apple dumplings",
+      "apple",
+      "dumplings"
+    ]
+  },
+  { 
+    image: "./images/desserts/sb-bars.png",
+    published: "20 October 2024",
+    name: "Starbucks Cranberry Bliss Bars",
+    description: "This recipe combines a moist, buttery blondie with sweet cranberries, a hint of orange zest, and a luscious frosting, mirroring the favorite holiday treat.",
+    author: "Erika Busz",
+    keywords: [
+      "starbucks",
+      "starbucks",
+      "cranberry bliss bars",
+      "cranberry",
+      "bliss",
+      "bars"
+    ]
+  },
+  { 
+    image: "./images/desserts/cf-original.png",
+    published: "20 October 2024",
+    name: "Cheesecake Factory OG Cheesecake",
+    description: "This recipe serves up a creamy, velvety cheesecake with a classic graham cracker crust, embodying the rich, indulgent flavor of the famous dessert.",
+    author: "Lauren Pahmeier",
+    keywords: [
+      "cheesecake factory",
+      "cheesecake",
+      "factory",
+      "original",
+      "og"
+    ]
+  },
+  { 
+    image: "./images/desserts/bundt-chocolate.jpg",
+    published: "20 October 2024",
+    name: "Nothing Bundt Chocolate Cake",
+    description: "This recipe brings the decadence of a rich, moist chocolate cake topped with luscious cream cheese frosting, perfecting the beloved bakery treat.",
+    author: "Kelsey Dimberg",
+    keywords: [
+      "cracker barrel",
+      "apple dumplings",
+      "apple",
+      "dumplings"
+    ]
+  },
+  { 
+    image: "./images/desserts/tiramisu.png",
+    published: "20 October 2024",
+    name: "Olive Garden Tiramisu",
+    description: "This recipe recreates the luscious, coffee-soaked layers of mascarpone and delicate ladyfingers, dusted with cocoa for a taste of Italian indulgence.",
+    author: "Linda Finn",
+    keywords: [
+      "olive garden",
+      "tiramisu",
+      "coffee",
+      "mascarpone",
+      "ladyfingers"
+    ]
+  },
+  { 
+    image: "./images/desserts/cream-pie.png",
+    published: "20 October 2024",
+    name: "Oatmeal Cream Pies",
+    description: "This recipe offers soft, chery oatmeal cookes sandwiched with a rich, creamy filling, capturing the nostalgic delight of the classic treat.",
+    author: "Crystal Schlueter",
+    keywords: [
+      "oatmeal",
+      "cream pies",
+      "oatmeal cream pies",
+      "oatmeal cookies",
+      "cream",
+      "pies"
+    ]
+  },
+  { 
+    image: "./images/desserts/maple-blondie.png",
+    published: "20 October 2024",
+    name: "Applebee's Maple Butter Blondie",
+    description: "A warm, buttery blondies infused with rich maple flavor, topped with a scoop of vanilla ice cream and drizzled with warm maple butter sauce.",
+    author: "Ollie Parker",
+    keywords: [
+      "applebee's",
+      "applebees",
+      "maple butter blondie",
+      "maple",
+      "butter",
+      "blondie"
+    ]
+  },
+  { 
+    image: "./images/desserts/lemon-pie.png",
+    published: "20 October 2024",
+    name: "Bakers Square Lemon Supreme Pie",
+    description: "This recipe combines a zesty lemon filling with a creamy layer atop a buttery crust, delivering a refreshing, citrusy delight just like the original.",
+    author: "Glenna Tooman",
+    keywords: [
+      "bakers square",
+      "lemon supreme pie",
+      "lemon",
+      "supreme",
+      "pie"
+    ]
+  },
+]
